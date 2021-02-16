fa1a6b7a7e906f83e57993dfc0cb93d789b2d8ac
pabloschonwiesner <pabloschonwiesner@gmail.com>
2021-02-16 19:20:46 -0300

    ejercicio con ejs terminado

--- src/views/index.ts
+++ src/views/index.ts
@@ -3,10 +3,8 @@ import producto from '../index'
 
 const app = express()
 
-app.set('view engine', 'pug')
+app.set('view engine', 'ejs')
 app.set('views', __dirname)
-
-
 
 let imagenes = [
   { path: "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-512.png", label: 'Calculadora' },
@@ -20,32 +18,29 @@ let imagenes = [
   { path: "https://cdn3.iconfinder.com/data/icons/education-209/64/bag-pack-container-school-512.png", label: 'Mochila' },
 ]
 
+function selectedOption (value: any, options: any) {
+  let items = ''
+  
+  imagenes.forEach( i => {
+    if(i.path == value) {
+      items = `${items}<option value="${i.path}" selected>${i.label}</option>`
+    } else {
+      items = `${items}<option value="${i.path}">${i.label}</option>`
+    }
+  })
+  return items
+}
+
 app.get('/vista', (req, res) => {
   res.render('productos', { productos: producto.getAll() })
 })
 
 app.get('/nuevoProducto', (req, res) => {
-  res.render('nuevoProducto', {producto: {id: undefined, title: '', price: 0, thumbnail: ''}, imagenes, filters: {
-    'selectedOption': function() {}
-  }})
+  res.render('nuevoProducto', {producto: {id: undefined, title: '', price: 0, thumbnail: ''}, imagenes, selectedOption })
 })
 
 app.get('/editarProducto/:id', (req, res) => {
-  res.render('editarProducto', { producto: producto.getOne(+req.params.id), imagenes,  filters: { 'selectedOption': function (value: any, options: any) { 
-    let items = ''
-    
-    imagenes.forEach( i => {
-      if(i.path == value) {
-        items = `${items}<option value="${i.path}" selected>${i.label}</option>`
-      } else {
-        items = `${items}<option value="${i.path}">${i.label}</option>`
-      }
-    })
-    
-    
-      return items
-    }
-  }})
+  res.render('editarProducto', { producto: producto.getOne(+req.params.id), imagenes, selectedOption })
 })
 
 export default app
